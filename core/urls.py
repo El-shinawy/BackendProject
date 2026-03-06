dca4ad0a55e6419380b7b3fe3357abb65f259c48
Mohamed Ebrahim <mohamedelshinawy14@gmail.com>
2026-03-06 04:08:31 +0200

    Refactor URL routing and add new endpoints

--- core/urls.py
+++ core/urls.py
@@ -1,32 +1,9 @@
-# from django.urls import path, include
-# from rest_framework.routers import DefaultRouter
-# from .views import *
-
-# router = DefaultRouter()
-# router.register('users', UserViewSet)
-# router.register('hospitals', HospitalViewSet)
-# router.register('doctors', DoctorViewSet)
-# router.register('chronic-diseases', ChronicDiseaseViewSet)
-# router.register('user-chronic-diseases', UserChronicDiseaseViewSet)
-# router.register('patients-profile', PatientMedicalProfileViewSet)
-# router.register('donors-profile', DonorMedicalProfileViewSet)
-# router.register('appointments', AppointmentViewSet)
-# router.register('organs', OrganViewSet)
-# router.register('organ-matches', OrganMatchingViewSet)
-# router.register('surgeries', SurgeryViewSet)
-# router.register('UserReport', MRIReportViewSet)
-# router.register('patient-priorities', PatientPriorityViewSet)
-# router.register('medicines', MedicineViewSet)
-# router.register('patient-medicines', PatientMedicineViewSet)
-# router.register('alerts', AlertViewSet)
-
-# urlpatterns = [
-#     path('api/', include(router.urls)),
-# ]
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from .views import *
+
 router = DefaultRouter()
+# router.register(r'ministry', MinistryViewSet, basename='ministry'),
 router.register(r'users', UserViewSet, basename='user')
 router.register(r'hospitals', HospitalViewSet, basename='hospital')
 router.register(r'doctors', DoctorViewSet, basename='doctor')
@@ -42,19 +19,22 @@ router.register(r'UserReport', UserReportViewSet, basename='UserReport')
 router.register(r'surgery-reports', SurgeryReportViewSet, basename='surgery-reports')
 router.register(r'patient-priority', PatientPriorityViewSet, basename='patient-priority')
 router.register(r'alerts', AlertViewSet, basename='alert')
-# router.register(r'vital-signs', VitalSignViewSet, basename='vital-signs')
-
-
-
-
+router.register(r'hospital-alerts', HospitalAlertViewSet, basename='hospital-alert')
+router.register(r'allergies', AllergyViewSet, basename='allergy')
+router.register(r'medicines', MedicineViewSet, basename='medicine')
+router.register(r'search/patients', PatientSearchViewSet, basename='patient-search')
+router.register(r'search/donors', DonorSearchViewSet, basename='donor-search')
 
 urlpatterns = [
     path('', include(router.urls)),
+
     path('register/', RegisterUserView.as_view(), name='register-user'),
     # path('login/', LoginUserView.as_view(), name='login-user'),
     path('logout/', LogoutUserView.as_view(), name='logout-user'),
     path('hospital/register/', HospitalRegisterView.as_view(), name='hospital-register'),
     # path('hospital/login/', HospitalLoginView.as_view(), name='hospital-login'),
-    path('login/', UnifiedLoginView.as_view(), name='unified-login')
+    path('login/', UnifiedLoginView.as_view(), name='unified-login'),
+    path('hospital/change-password/', ChangeHospitalPasswordView.as_view(), name='change_password'),
+
     # path('users/search_by_organ/', search_users_by_organ, name='search_by_organ'),
 ]
